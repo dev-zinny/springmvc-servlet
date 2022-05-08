57e2c1efa9dddaa9ae022626299809cd848d8648
dev-zinny <dev.zinny@gmail.com>
2022-05-08 20:45:24 +0900

    생성자 추가

--- src/main/java/hello/servlet/web/frontcontroller/ModelView.java
+++ src/main/java/hello/servlet/web/frontcontroller/ModelView.java
@@ -11,4 +11,8 @@ public class ModelView {
 
     private String viewName;
     private Map<String, Object> model = new HashMap<>();
+
+    public ModelView(String viewName) {
+        this.viewName = viewName;
+    }
 }
